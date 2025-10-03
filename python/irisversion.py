b59f992e3beec8021a42c65b2a23a9972f4dc66b
wolfman0719 <hsato0719@icloud.com>
2025-10-03 17:19:50 +0900

    Add files via upload

--- python/irisversion.py
+++ python/irisversion.py
@@ -1,5 +1,5 @@
 import iris
-
+import os
 # Open a connection to the server
 args = {'hostname':'127.0.0.1', 'port':1972,
     'namespace':'USER', 'username':'_SYSTEM', 'password':'SYS'
@@ -10,3 +10,9 @@ irispy = iris.createIRIS(conn)
 className =  '%SYSTEM.Version'
 stringVal = irispy.classMethodString(className,'GetVersion')
 print('IRIS Version = ' + stringVal)
+className =  '%SYSTEM.OBJ'
+dir = os.getcwd()
+fileName = dir.split('/python')[0] + '/src/Samples/ADBK.cls'
+status = irispy.classMethodObject(className,'Import',fileName,'ck')
+# error = irispy.classMethodString('%SYSTEM.Status','GetOneErrorText',status)
+# print('Error = ' + error)
